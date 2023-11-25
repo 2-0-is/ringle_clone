82254a0226484f476bdd0db25fa23820e30f4486
2-0-is <yuye2002@snu.ac.kr>
2023-11-26 07:36:38 +0900

    implement small calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Header from "../components/header";
+import DayPick from "@/components/dayPick";
+import "react-day-picker/dist/style.css";
 
 export default function Home() {
   return (
     <div>
       <main className="flex min-h-screen min-w-full flex-col items-center container">
         <Header />
-        <div>main page</div>
+        <div className="flex flex-row items-center relative h-full w-full">
+          <DayPick />
+          <div />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/dayPick.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import "react-day-picker/dist/style.css";
+import { DateRange, DayPicker } from "react-day-picker";
+
+const DayPick = () => {
+  const defaultSelected: DateRange = {
+    from: new Date("2023.11.26"),
+    to: new Date("2023.12.2"),
+  };
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+
+  return (
+    <div className="flex-col justify-center mr-auto">
+      <div className="h-[110px]" />
+      <DayPicker
+        mode="range"
+        modifiersClassNames={{ selected: "bg-purple-300" }}
+        selected={defaultSelected}
+        onSelect={setRange}
+        showOutsideDays
+      />
+      <div className="mt-[48px] pt-[16px] text-center">
+        <div className="text-xs text-gray-300">신청 방식 변경</div>
+        <button className="border border-gray-500 p-2 mx-auto mt-2 text-sm w-fit rounded-md">
+          <div>튜터 먼저 선택</div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DayPick;
